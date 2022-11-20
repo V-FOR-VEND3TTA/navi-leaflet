5323d66e594f5900d7ef2f8836e104fe40f93b2c
Geo-hot <55334979+Shrapnel-max@users.noreply.github.com>
2022-11-20 05:16:25 +0200

    Added layers to map

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-// Coordinates for Carnival Mall
-var map = L.map('mapid').setView([-26.251581565897556, 28.318399382254157], 13);
+// Different types of map layers
+var stamenMap = L.tileLayer.provider('Stamen.Watercolor');
+var osMap = L.tileLayer.provider('OpenStreetMap.Mapnik');
+var imageryMap = L.tileLayer.provider('Esri.WorldImagery');
 
+// To allow switching functionality of map layers
+var baseMaps = {
+    OSM: osMap,
+    'Stamen Watercolor': stamenMap,
+    'World Imagery': imageryMap
+}
+
+// Coordinates for Carnival Mall
+// var map = L.map('mapid').setView([-26.251581565897556, 28.318399382254157], 13);
+var map = L.map('mapid', {
+    center: [-26.251581565897556, 28.318399382254157],
+    zoom: 13,
+    layers: [osMap]
+});
+
+var geoServerIPPort = 'localhost:5500';
+var geoServerWorkSpace = 'GIS';
+var stateLayerName = 'GIS:ind_st';
+
+var southAfricaProvinceLayer = L.tileLayer.wms (
+    "http://" + geoServerIPPort + "/geoserver" + geoServerWorkSpace + "/wms",
+    {
+        layers: stateLayerName,
+        format: "image/png",
+        transparent: true,
+        version: "1.1.0",
+        tiled: true,
+    }
+)
 // Marker for the exact location of Carnival Mall
 var marker = L.marker([-26.251581565897556, 28.318399382254157]).addTo(map);
 var marker2 = L.marker([-26.275042497360214, 28.453913539642063]).addTo(map);
@@ -30,7 +61,10 @@ In this example we’ll use the mapbox/streets‐v11 tiles from Mapbox’s Stati
 Mapbox, you must also request an access token). Because this API returns 512x512 tiles by default (instead of
 256x256), we will also have to explicitly specify this and o set our zoom by a value of -1. */
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+/*L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+*/
+
+var mapLayers = L.control.layers(baseMaps).addTo(map);
